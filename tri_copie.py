11401894e1220006625e8c7d4bf310d1360329c5
camille-310 <camille.cesbron9@gmail.com>
2024-09-18 15:31:42 +0200

    Update tri_copie.py

--- tri_copie.py
+++ tri_copie.py
@@ -3,7 +3,7 @@ import sys, re
 
 """
     Soit L le type liste dont les Ã©lÃ©ments sont soit tous de type int, soit tous de type L.
-
+bcvcvbsbefdbgfdfbnd dn
     Ce programme lit des liste de type L sur l'entrÃ©e standard, au format
     [ [ 1 2 ] [ [ 2 3 4 ] [ 5 4 3 2 ] [ [ 3 1 ] [ 2 ] ] ] [ 0 9 ] ]
     et sort cette liste dans laquelle les sous-listes d'entiers sont triÃ©es.
